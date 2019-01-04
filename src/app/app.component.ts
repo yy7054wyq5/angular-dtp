f845eea1b6b77987aa3ad2d361073b8204c0d82d
wuyang <yang_wu@loongjoy.com>
2019-01-04 17:25:52 +0800

    nothing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,9 @@ import {
   AfterContentInit,
   ViewEncapsulation
 } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, fromEvent, interval, of, concat, merge, zip } from 'rxjs';
 import { DDirective } from './share/d.directive';
+import { mergeMapTo, mergeMap, map } from 'rxjs/operators';
 // import { AComponent } from './share/a/a.component';
 
 @Component({
@@ -22,7 +23,7 @@ import { DDirective } from './share/d.directive';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-test';
   isCollapsed = false;
   isReverseArrow = false;
@@ -48,6 +49,24 @@ export class AppComponent {
     this.fetch((data: any[]) => {
       this.rows = data.splice(1, 20);
     });
+  }
+
+  ngOnInit() {
+    const clicks = of(1, 2, 3, 4);
+    const result = clicks.pipe(mergeMapTo(of(5555)));
+    result.subscribe(x => console.log(x));
+
+    // const letters = of('a', 'b', 'c');
+    // const result = letters.pipe(mergeMap(x => interval(1000).pipe(map(i => x + i))));
+    // result.subscribe(x => console.log(x));
+
+    const o1 = of(1);
+    const o2 = of([1]);
+    zip(o1, o2)
+      // .pipe(map(x => x))
+      .subscribe(x => {
+        console.log(x);
+      });
   }
 
   onPage(event) {
