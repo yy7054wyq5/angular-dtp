23caa7b6fbe05b7826ebc9b909ee2518c20df738
wuyang <349413799@qq.com>
2019-03-29 21:21:48 +0800

    feat: ..

--- src1/app/eagerly-loaded/services/eagerly-module.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { EagerlyLoadedModule } from '../eagerly-loaded.module';
-
-@Injectable({
-	providedIn: EagerlyLoadedModule
-})
-export class EagerlyModuleService {
-	constructor() {}
-}
--- src1/app/lazy-loaded/services/lazy-loaded.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { LazyLoadedModule } from '../lazy-loaded.module';
-
-@Injectable({
-	providedIn: LazyLoadedModule
-})
-export class LazyLoadedService {
-	constructor() {}
-}
