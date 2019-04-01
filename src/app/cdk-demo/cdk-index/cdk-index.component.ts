4daf0e040d563c39de6483d95ec9abb506f9d056
wuyang <349413799@qq.com>
2019-04-01 17:55:50 +0800

    feat: nothing

--- src/app/cdk-demo/cdk-index/cdk-index.component.ts
+++ src/app/cdk-demo/cdk-index/cdk-index.component.ts
@@ -1,6 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
+function rnd(seed) {
+  seed = (seed * 9301 + 49297) % 233280; //为何使用这三个数?
+  return seed / 233280.0;
+}
+
+function rand(number) {
+  const today = new Date();
+  const seed = today.getTime();
+  return Math.ceil(rnd(seed) * number);
+}
+
+class Item {
+  width: string;
+  height: string;
+  name: number;
+
+  size = 20;
+
+  constructor(w,h){
+    this.name = rand(w+h+rand(111));
+    this.width = w * this.size + 'px';
+    this.height = h * this.size + 'px';
+  }
+}
+
 @Component({
   selector: 'scm-cdk-index',
   templateUrl: './cdk-index.component.html',
@@ -8,10 +33,18 @@ import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 })
 export class CdkIndexComponent implements OnInit {
 
-  data = [1,2,3,4];
+  data = [];
   data1 = [];
 
-  constructor() { }
+  constructor() { 
+    this.data = ((n)=> {
+      const arr = [];
+      for (let index = 0; index < n; index++) {
+        arr.push(new Item(index+1, index + 2));
+      }
+      return arr;
+    })(10);
+  }
 
   ngOnInit() {
   }
